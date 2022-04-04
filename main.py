add8bfdc303255ea48f9eea243405deeff40b1d7
moinul75 <102654562+moinul75@users.noreply.github.com>
2022-04-04 15:55:36 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#break and continue
+i = 10
+while i<=100:
+    if i==20:
+        break
+    print(i)
+    i = i+1
+    
+    
+i= 10
+while i<=100:
+    if i == 20:
+        continue
+    print("hello")
